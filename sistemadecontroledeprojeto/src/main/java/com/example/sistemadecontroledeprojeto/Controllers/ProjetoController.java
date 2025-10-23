dc248768f45e62ee1dd53aeb01457e721c6f93d7
Luckvers <156406555+Luckvers@users.noreply.github.com>
2025-10-22 21:53:21 -0300

    primeiro commit

--- sistemadecontroledeprojeto/src/main/java/com/example/sistemadecontroledeprojeto/Controllers/ProjetoController.java
+++ sistemadecontroledeprojeto/src/main/java/com/example/sistemadecontroledeprojeto/Controllers/ProjetoController.java
@@ -1,5 +1,25 @@
 package com.example.sistemadecontroledeprojeto.Controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.sistemadecontroledeprojeto.Services.ProjetoService;
+import com.example.sistemadecontroledeprojeto.dtos.ProjetoRequestDTO;
+
+@RestController
+@RequestMapping("/projetos")
 public class ProjetoController {
-    
+    private ProjetoService projetoService;
+
+    public ProjetoController(ProjetoService projetoService) {
+        this.projetoService = projetoService;
+    }
+
+    @PostMapping
+    public void adicionar(@RequestBody ProjetoRequestDTO projetoRequestDTO) {
+        projetoService.salvar(projetoRequestDTO);
+        
+    }
 }
--- sistemadecontroledeprojeto/src/main/java/com/example/sistemadecontroledeprojeto/Controllers/SetorController.java
+++ sistemadecontroledeprojeto/src/main/java/com/example/sistemadecontroledeprojeto/Controllers/SetorController.java
@@ -1,5 +1,25 @@
 package com.example.sistemadecontroledeprojeto.Controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.sistemadecontroledeprojeto.Services.SetorService;
+import com.example.sistemadecontroledeprojeto.dtos.SetorRequestDTO;
+
+@RestController
+@RequestMapping("/setores")
 public class SetorController {
-    
+    private SetorService setorService;
+
+    public SetorController(SetorService setorService) {
+        this.setorService = setorService;
+    }
+
+     @PostMapping
+    public void adicionar(@RequestBody SetorRequestDTO setorRequestDTO) {
+        setorService.salvar(setorRequestDTO);
+        
+    }
 }
